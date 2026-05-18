dbc36592f828159a637915d9ef53e2d5a436bfc1
jccampos_2108 <Vale2007$>
2026-05-18 10:20:30 -0400

    update

--- App/Modules/InscripcionCurso/list.php
+++ App/Modules/InscripcionCurso/list.php
@@ -5,7 +5,7 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css" />
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css" />
 
-<h2 class="text-3xl font-semibold text-gray-800 mb-6">Gestión de Inscripcion Talleres</h2>
+<h2 class="text-3xl font-semibold text-gray-800 mb-6">Gestión de Inscripcion Talleres / Cursos</h2>
 
 <div class="flex justify-end mb-4">
     <a href="<?php echo BASE_URL; ?>inscripcion_curso/create" class="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
@@ -19,7 +19,7 @@
         <thead>
             <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
                 <th class="py-3 px-6 text-left">ID</th>
-                <th class="py-3 px-6 text-left">Taller Abierto</th>
+                <th class="py-3 px-6 text-left">Taller / Curso Abierto</th>
                 <th class="py-3 px-6 text-left">Alumno</th>
                 <th class="py-3 px-6 text-left">Estatus</th>
                 <th class="py-3 px-6 text-center">Acciones</th>
